936d45dce882c4ae2704b969562971b7d77b26cb
xNemix <46863616+xNemix@users.noreply.github.com>
2022-08-10 10:38:33 +0200

    Update js

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const onClickPortfolio = () => {
     ><img
     src="https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
     /></a>
-    
-      <h1>Kommer senere!</h1>
-    
-
+    <h1>Kommer senere!</h1>
     <audio autoplay loop>
       <source src="audio/rickroll.mp3" type="audio/mpeg">
     Your browser does not support the audio element.
