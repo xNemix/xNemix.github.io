504e509270176e01afada3b688706aab8378d495
xNemix <46863616+xNemix@users.noreply.github.com>
2022-08-10 10:31:34 +0200

    Change stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+const onClickPortfolio = () => {
+  document.getElementById('container1').innerHTML = /*HTML*/ `
+    <a href="https://github.com/xNemix" target="_blank"
+    ><img
+    src="https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
+    /></a>
+
+    <audio autoplay loop>
+      <source src="audio/rickroll.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+    </audio>`;
+};
+
+const onClickGithub = () => {
+  document.getElementById('container1').innerHTML = /*HTML*/ `
+    `;
+};
