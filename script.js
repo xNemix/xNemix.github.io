96f70043607c0c8b1fc42e15bef8ffb5ce2fa103
xNemix <46863616+xNemix@users.noreply.github.com>
2022-08-10 10:35:29 +0200

    Test

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const onClickPortfolio = () => {
     ><img
     src="https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
     /></a>
+    
+      <h1>Kommer senere!</h1>
+    
 
     <audio autoplay loop>
       <source src="audio/rickroll.mp3" type="audio/mpeg">
